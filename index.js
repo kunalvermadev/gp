c4d4c3864caf576022be8613a511fd1252d18221
kunalverma94 <34855574+kunalverma94@users.noreply.github.com>
2018-11-30 15:06:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+var c = 0;
+var t = []
+function hove(e) {
+    var u = 'url(XX)'.replace('XX', e.src);
+    document.getElementById("bgg").style.background = u;
+}
+window.onload = function () {
+    foo();
+    window.addEventListener("scroll", (e) => {
+        var g = ((window.innerHeight + window.scrollY) + 100 >= document.body.offsetHeight);
+        if (g) {
+            foo();
+        }
+    }
+    );
+}
+
+function foo() {
+    return new Promise((v) => {
+        t = Array.apply(null, {
+            length: 15
+        }).map(Number.call, (a) => {
+            c++;
+            return {
+                el: document.querySelector(".card_box").cloneNode(true),
+                i: "https://avatars1.githubusercontent.com/u/" + c
+            };
+        }
+        );
+        t.forEach((v, i) => {
+            setTimeout(() => {
+                document.querySelector(".flex_card").append(v.el);
+            }
+                , 500 + (i * 200));
+        }
+        );
+        v(88);
+
+    }
+    ).then((_c) => {
+        t.forEach((v, i) => {
+            setTimeout(() => {
+                v.el.querySelector("img").src = v.i;
+            }
+                , 500 + (i * 400));
+
+            t = [];
+        }
+        );
+        ;
+
+    }
+    );
+
+}
